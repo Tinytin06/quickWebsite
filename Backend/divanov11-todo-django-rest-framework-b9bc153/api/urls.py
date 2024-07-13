35fc4ef77b47d2e39149ac212dc2785770db0e6b
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2024-07-12 20:35:57 -0700

    making tasks into visited and button clicked

--- Backend/divanov11-todo-django-rest-framework-b9bc153/api/urls.py
+++ Backend/divanov11-todo-django-rest-framework-b9bc153/api/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.apiOverview, name="api-overview"),
-	path('task-list/', views.taskList, name="task-list"),
-	path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
+	path('/amounts/visited/', views.taskList, name="task-list"),
+	path('/amounts/clicked/', views.taskDetail, name="task-detail"),
 	path('task-create/', views.taskCreate, name="task-create"),
 
 	path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
--- Backend/divanov11-todo-django-rest-framework-b9bc153/api/views.py
+++ Backend/divanov11-todo-django-rest-framework-b9bc153/api/views.py
@@ -11,8 +11,8 @@ from .models import Task
 @api_view(['GET'])
 def apiOverview(request):
 	api_urls = {
-		'List':'/task-list/',
-		'Detail View':'/task-detail/<str:pk>/',
+		'Visited':'/amounts/visited/',
+		'Button Clicked':'/amounts/clicked/',
 		'Create':'/task-create/',
 		'Update':'/task-update/<str:pk>/',
 		'Delete':'/task-delete/<str:pk>/',
@@ -21,13 +21,13 @@ def apiOverview(request):
 	return Response(api_urls)
 
 @api_view(['GET'])
-def taskList(request):
+def visited(request):
 	tasks = Task.objects.all().order_by('-id')
 	serializer = TaskSerializer(tasks, many=True)
 	return Response(serializer.data)
 
 @api_view(['GET'])
-def taskDetail(request, pk):
+def buttonPressed(request, pk):
 	tasks = Task.objects.get(id=pk)
 	serializer = TaskSerializer(tasks, many=False)
 	return Response(serializer.data)
